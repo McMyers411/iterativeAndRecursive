c5c387659cb7f6fbee1fc434bc5ddfedd8e8dfbb
McMyers411 <nilegend@gmail.com>
2017-02-27 11:03:49 -0500

    Last Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
+        System.out.println("Howdy, please enter your first number");
+        int m = input.nextInt();
+        System.out.println("Please enter your second number");
+        int n = input.nextInt();
+        int i = Euclid.iterative(m, n);
+        int j = Euclid.recursive(m, n);
+        System.out.println("recursive (" + m + ", " + n + ") = " + j );
+        System.out.println("iterative (" + m + ", " + n + ") = " + i );
 
     }
 }
